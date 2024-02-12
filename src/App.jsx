2a57840d05d43e48b834d3f3c58108a5331b62ae
marck0101 <marcos.henrique.a7x@gmail.com>
2024-02-12 10:39:34 -0300

    ajuste routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,3 @@
-/**
-=========================================================
-* Material Kit 2 PRO React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-pro-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 import { useEffect } from "react";
 
@@ -56,7 +42,7 @@ export default function App() {
       <CssBaseline />
       <Routes>
         {getRoutes(routes)}
-        <Route path="/presentation" element={<Presentation />} />
+        {/* <Route path="/presentation" element={<Presentation />} /> */}
         {/* <Route path="*" element={<Navigate to="/presentation" />} /> */}
         {/* <Route path="*" element={<Navigate to="/pages/landing-pages/rental" />} /> */}
         <Route path="*" element={<Navigate to="/" />} />
